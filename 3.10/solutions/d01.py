30c92f7d20b062a13391cee6b9b909960d578303
Sean Malloy <techfan321@gmail.com>
2021-09-11 18:27:15 -0400

    Formatting

--- 3.10/solutions/d01.py
+++ 3.10/solutions/d01.py
@@ -4,13 +4,13 @@ def main(in_file: TextIOWrapper):
     modules = list(map(int, in_file.readlines()))
     t1 = t2 = 0
     for m in modules:
-        curr = (m//3 - 2)
+        curr = m//3 - 2
         t1 += curr
         leftover = 0
         while curr > 0:
             leftover += curr
             m = curr
-            curr = (m//3 - 2)
+            curr = m//3 - 2
         t2 += leftover
     print(f'Part 1: {t1}')
     print(f'Part 2: {t2}')
