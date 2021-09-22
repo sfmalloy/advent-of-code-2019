44b70863473fb514fcf589ffcc60552020bfd369
Sean Malloy <techfan321@gmail.com>
2021-09-22 12:28:51 -0400

    Removed misc intcode files

--- 3.10/euler1_short.py
+++
@@ -1,6 +0,0 @@
-# print(sum(map(lambda x:x if 0 in[x%3,x%5] else 0, list(range(1000)))))
-
-asum=lambda n:n*(n+1)//2
-gsum=lambda l:lambda x:x*asum((l-1)//x)
-psum=gsum(10**100000)
-print(psum(3)+psum(5)-psum(15))
