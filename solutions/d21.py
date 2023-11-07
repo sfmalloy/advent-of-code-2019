82d0ab807aa97b29493d20ea147ea446e4aed8bd
Sean Malloy <techfan321@gmail.com>
2023-11-06 20:41:37 -0500

    Did this one by hand

--- solutions/d21.py
+++ solutions/d21.py
@@ -11,6 +11,8 @@ def survey(comp: Intcode, asm: str):
 
 
 def main(file: TextIOWrapper):
+    # did this entire day by hand for fun
+
     comp = Intcode(file.readline())
 
     # instructions = {'AND','OR','NOT'}
@@ -18,23 +20,30 @@ def main(file: TextIOWrapper):
     # outputs      = {'T','J'}
     # jump distance is 4 tiles
     walk = '\n'.join('''
-        NOT B T
-        AND C T
-        AND T J
         NOT C T
         AND D T
         OR T J
         NOT A T
-        AND C T
         OR T J
         WALK
     '''.lstrip().split('\n        '))
     
+    #             1   2   3   4   5   6   7   8   9
     # outputs = {'A','B','C','D','E','F','G','H','I','T','J'}
     # everything else is the same
     run = '\n'.join('''
+        NOT B T
+        AND D T
+        OR T J
+        NOT C T
+        AND D T
+        AND H T
+        OR T J
+        NOT A T
+        AND D T
+        OR T J
         RUN
     '''.lstrip().split('\n        '))
 
     print(survey(comp, walk).split('\n')[-1])
-    print(survey(comp, run))
+    print(survey(comp, run).split('\n')[-1])
