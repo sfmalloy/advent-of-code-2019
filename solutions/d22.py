da9c07fa6595f5ecd87c697a632d7841d44a74ff
Sean Malloy <techfan321@gmail.com>
2023-11-08 17:52:57 -0500

    New stackless part 1

--- solutions/d22.py
+++ solutions/d22.py
@@ -2,115 +2,35 @@ from io import TextIOWrapper
 from typing import Self, Optional
 from dataclasses import dataclass
 
-# Classes
-@dataclass
-class Card:
-    val: int
-    deck_size: int
-    next: Optional[Self] = None
-
-    def print(self):
-        seen = set()
-        curr = self
-        seen.add(curr.val)
-        while True:
-            print(curr.val, end=' ')
-            curr = curr.next
-            if curr == self:
-                break
-            seen.add(curr.val)
-        print()
-    
 
 def main(file: TextIOWrapper):
     commands = file.readlines()
-    part1(commands)
+    print(part1(commands, 10007, 2019))
 
 
-def part1(commands: list[str]):
-    top, bottom = new_deck(10007)
-
+def part1(commands: list[str], N: int, pos: int):
     for cmd in commands:
         words = cmd.split()
         match words:
-            case ['cut', i]:
-                top, bottom = cut(top, bottom, int(i))
+            case ['cut', x]:
+                pos = cut(pos, int(x), N)
             case ['deal', 'into', 'new', 'stack']:
-                top, bottom = reverse(top, bottom)
-            case ['deal', 'with', 'increment', i]:
-                top, bottom = deal(top, int(i))
-
-    curr = top
-    pos = 0
-    while curr.val != 2019:
-        curr = curr.next
-        pos += 1
-    print(pos)
+                pos = new_stack(pos, N)
+            case ['deal', 'with', 'increment', x]:
+                pos = deal(pos, int(x), N)
+        print(words, pos)
+    return pos
 
 
-
-# Helper Functions
-def new_deck(deck_size: int):
-    top = Card(0, deck_size)
-    bottom = top
-    for val in range(1, deck_size):
-        tmp = Card(val, deck_size)
-        bottom.next = tmp
-        bottom = bottom.next
-    bottom.next = top
-    return top, bottom
+def new_stack(pos: int, N: int) -> int:
+    return -(pos + 1) % N
 
 
-def cut(top: Card, bottom: Card, idx: int) -> Card:
-    if idx < 0:
-        idx += top.deck_size
-    cut_point = top
-    for _ in range(idx-1):
-        cut_point = cut_point.next
-
-    n = cut_point.next
-    cut_point.next = n
-    bottom.next = top
-    return n, cut_point
+def cut(pos: int, x: int, N: int) -> int:
+    if x < 0:
+        x += N
+    return (pos + (N - x)) % N
 
 
-def reverse(top: Card, bottom: Card):
-    prev = bottom
-    curr = top
-    for _ in range(top.deck_size):
-        tmp = curr.next
-        curr.next = prev
-        prev = curr
-        curr = tmp
-    return bottom, top
-
-
-def deal(top: Card, inc: int):
-    t, b = new_deck(top.deck_size)
-    old = top
-    new = t
-    for _ in range(top.deck_size):
-        new.val = old.val
-        old = old.next
-        for _ in range(inc):
-            new = new.next
-    return t, b
-
-
-def get(top: Card, idx: int):
-    curr = top
-    for _ in range(idx):
-        curr = curr.next
-    return curr
-
-
-'''
-119315717514047 cards
-101741582076661 shuffles
-
-def new_pos(pos, x, N):
-    if pos % x == 0:
-        return pos // x
-    return ((pos % x) * (pos // x)) + ((N // x) + (x - (N % x))) * (pos % x)
-
-'''
+def deal(pos: int, x: int, N: int) -> int:
+    return (x * pos) % N
